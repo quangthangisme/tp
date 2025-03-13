4d475eeab796c179c68affcaa242ac3df86d9ab9
NHT020305 <tungtun020305@gmail.com>
2025-03-13 21:38:49 +0800

    Add aliases for exit command

--- src/main/java/seedu/address/logic/commands/ExitCommand.java
+++ src/main/java/seedu/address/logic/commands/ExitCommand.java
@@ -7,7 +7,10 @@ import seedu.address.model.Model;
  */
 public class ExitCommand extends Command {
 
-    public static final String COMMAND_WORD = "exit";
+    public static final String COMMAND_WORD_EXIT = "exit";
+    public static final String COMMAND_WORD_BYE = "bye";
+    public static final String COMMAND_WORD_QUIT = "quit";
+    public static final String COMMAND_WORD_KILL = "kill";
 
     public static final String MESSAGE_EXIT_ACKNOWLEDGEMENT = "Exiting Address Book as requested ...";
 
