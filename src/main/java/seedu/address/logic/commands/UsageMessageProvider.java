c6b3d293863fe6d57f09217fbe27625153b4b389
NHT020305 <tungtun020305@gmail.com>
2025-03-18 04:45:04 +0800

    Handle help <feature> command

--- src/main/java/seedu/address/logic/commands/UsageMessageProvider.java
+++ src/main/java/seedu/address/logic/commands/UsageMessageProvider.java
@@ -5,8 +5,19 @@ import seedu.address.logic.commands.event.DeleteEventCommand;
 import seedu.address.logic.commands.event.DisplayEventInformationCommand;
 import seedu.address.logic.commands.event.ListEventCommand;
 import seedu.address.logic.commands.exceptions.CommandException;
-import seedu.address.logic.commands.person.*;
-import seedu.address.logic.commands.todo.*;
+import seedu.address.logic.commands.person.AddPersonCommand;
+import seedu.address.logic.commands.person.ClearPersonCommand;
+import seedu.address.logic.commands.person.DeletePersonCommand;
+import seedu.address.logic.commands.person.EditPersonCommand;
+import seedu.address.logic.commands.person.FindPersonCommand;
+import seedu.address.logic.commands.person.InfoPersonCommand;
+import seedu.address.logic.commands.person.ListPersonCommand;
+import seedu.address.logic.commands.todo.AddPersonToTodoCommand;
+import seedu.address.logic.commands.todo.AddTodoCommand;
+import seedu.address.logic.commands.todo.DeleteTodoCommand;
+import seedu.address.logic.commands.todo.DisplayTodoInformationCommand;
+import seedu.address.logic.commands.todo.ListTodoCommand;
+import seedu.address.logic.commands.todo.RemovePersonFromTodoCommand;
 
 import static seedu.address.logic.parser.CliSyntax.EVENT_COMMAND_WORD;
 import static seedu.address.logic.parser.CliSyntax.TODO_COMMAND_WORD;
@@ -27,37 +38,37 @@ public class UsageMessageProvider {
 
         switch (feature) {
 
-        case AddPersonCommand.COMMAND_WORD:
-            return AddPersonCommand.MESSAGE_USAGE;
+            case AddPersonCommand.COMMAND_WORD:
+                return AddPersonCommand.MESSAGE_USAGE;
 
-        case EditPersonCommand.COMMAND_WORD:
-            return EditPersonCommand.MESSAGE_USAGE;
+            case EditPersonCommand.COMMAND_WORD:
+                return EditPersonCommand.MESSAGE_USAGE;
 
-        case DeletePersonCommand.COMMAND_WORD:
-            return DeletePersonCommand.MESSAGE_USAGE;
+            case DeletePersonCommand.COMMAND_WORD:
+                return DeletePersonCommand.MESSAGE_USAGE;
 
-        case ClearPersonCommand.COMMAND_WORD:
-            return ClearPersonCommand.MESSAGE_USAGE;
+            case ClearPersonCommand.COMMAND_WORD:
+                return ClearPersonCommand.MESSAGE_USAGE;
 
-        case FindPersonCommand.COMMAND_WORD:
-            return FindPersonCommand.MESSAGE_USAGE;
+            case FindPersonCommand.COMMAND_WORD:
+                return FindPersonCommand.MESSAGE_USAGE;
 
-        case ListPersonCommand.COMMAND_WORD:
-            return ListPersonCommand.MESSAGE_USAGE;
+            case ListPersonCommand.COMMAND_WORD:
+                return ListPersonCommand.MESSAGE_USAGE;
 
-        case InfoPersonCommand.COMMAND_WORD:
-            return InfoPersonCommand.MESSAGE_USAGE;
+            case InfoPersonCommand.COMMAND_WORD:
+                return InfoPersonCommand.MESSAGE_USAGE;
 
-        case ExitCommand.COMMAND_WORD_EXIT, ExitCommand.COMMAND_WORD_BYE,
-             ExitCommand.COMMAND_WORD_QUIT, ExitCommand.COMMAND_WORD_KILL:
-            return ExitCommand.MESSAGE_USAGE;
+            case ExitCommand.COMMAND_WORD_EXIT, ExitCommand.COMMAND_WORD_BYE,
+                 ExitCommand.COMMAND_WORD_QUIT, ExitCommand.COMMAND_WORD_KILL:
+                return ExitCommand.MESSAGE_USAGE;
 
-        case HelpCommand.COMMAND_WORD:
-            return HelpCommand.MESSAGE_USAGE;
+            case HelpCommand.COMMAND_WORD:
+                return HelpCommand.MESSAGE_USAGE;
 
-        default:
-            throw new CommandException(String.format(
-                    UNKNOWN_FEATURE_MESSAGE, feature));
+            default:
+                throw new CommandException(String.format(
+                        UNKNOWN_FEATURE_MESSAGE, feature));
         }
     }
 
@@ -65,27 +76,27 @@ public class UsageMessageProvider {
 
         switch (todoFeature.split(" ")[1]) {
 
-        case AddTodoCommand.COMMAND_WORD:
-            return AddTodoCommand.MESSAGE_USAGE;
+            case AddTodoCommand.COMMAND_WORD:
+                return AddTodoCommand.MESSAGE_USAGE;
 
-        case DisplayTodoInformationCommand.COMMAND_WORD:
-            return DisplayTodoInformationCommand.MESSAGE_USAGE;
+            case DisplayTodoInformationCommand.COMMAND_WORD:
+                return DisplayTodoInformationCommand.MESSAGE_USAGE;
 
-        case ListTodoCommand.COMMAND_WORD:
-            return ListTodoCommand.MESSAGE_USAGE;
+            case ListTodoCommand.COMMAND_WORD:
+                return ListTodoCommand.MESSAGE_USAGE;
 
-        case DeleteTodoCommand.COMMAND_WORD:
-            return DeleteTodoCommand.MESSAGE_USAGE;
+            case DeleteTodoCommand.COMMAND_WORD:
+                return DeleteTodoCommand.MESSAGE_USAGE;
 
-        case AddPersonToTodoCommand.COMMAND_WORD:
-            return AddPersonToTodoCommand.MESSAGE_USAGE;
+            case AddPersonToTodoCommand.COMMAND_WORD:
+                return AddPersonToTodoCommand.MESSAGE_USAGE;
 
-        case RemovePersonFromTodoCommand.COMMAND_WORD:
-            return RemovePersonFromTodoCommand.MESSAGE_USAGE;
+            case RemovePersonFromTodoCommand.COMMAND_WORD:
+                return RemovePersonFromTodoCommand.MESSAGE_USAGE;
 
-        default:
-            throw new CommandException(String.format(
-                    UNKNOWN_FEATURE_MESSAGE, todoFeature));
+            default:
+                throw new CommandException(String.format(
+                        UNKNOWN_FEATURE_MESSAGE, todoFeature));
         }
     }
 
@@ -93,21 +104,21 @@ public class UsageMessageProvider {
 
         switch (eventFeature.split(" ")[1]) {
 
-        case AddEventCommand.COMMAND_WORD:
-            return AddEventCommand.MESSAGE_USAGE;
+            case AddEventCommand.COMMAND_WORD:
+                return AddEventCommand.MESSAGE_USAGE;
 
-        case DisplayEventInformationCommand.COMMAND_WORD:
-            return DisplayEventInformationCommand.MESSAGE_USAGE;
+            case DisplayEventInformationCommand.COMMAND_WORD:
+                return DisplayEventInformationCommand.MESSAGE_USAGE;
 
-        case ListEventCommand.COMMAND_WORD:
-            return ListEventCommand.MESSAGE_USAGE;
+            case ListEventCommand.COMMAND_WORD:
+                return ListEventCommand.MESSAGE_USAGE;
 
-        case DeleteEventCommand.COMMAND_WORD:
-            return DeleteEventCommand.MESSAGE_USAGE;
+            case DeleteEventCommand.COMMAND_WORD:
+                return DeleteEventCommand.MESSAGE_USAGE;
 
-        default:
-            throw new CommandException(String.format(
-                    UNKNOWN_FEATURE_MESSAGE, eventFeature));
+            default:
+                throw new CommandException(String.format(
+                        UNKNOWN_FEATURE_MESSAGE, eventFeature));
         }
     }
 }
