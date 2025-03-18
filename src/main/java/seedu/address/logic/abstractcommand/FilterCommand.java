70e59fd52f4a531be3be858203f15397323b26dd
raw-asparagus <66353964+raw-asparagus@users.noreply.github.com>
2025-03-18 22:52:08 +0800

    Add `filter` command abstract class

---
+++ src/main/java/seedu/address/logic/abstractcommand/FilterCommand.java
@@ -0,0 +1,51 @@
+package seedu.address.logic.abstractcommand;
+
+import static java.util.Objects.requireNonNull;
+
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+import seedu.address.logic.commands.CommandResult;
+import seedu.address.model.Model;
+import seedu.address.model.item.Item;
+import seedu.address.model.item.ItemManagerWithFilteredList;
+
+/**
+ * Abstract command for filtering items in the model that match a given complex {@code predicate}.
+ *
+ * @param <T> the type of {@code Item} being filtered, which must extend {@link Item}.
+ */
+public abstract class FilterCommand<T extends Item> extends ItemCommand<T> {
+
+    protected final Predicate<T> predicate;
+
+    /**
+     * Creates a {@code FilterCommand} to filter items that match the given {@code predicate}.
+     *
+     * @throws NullPointerException if {@code predicate} or {@code managerAndListGetter} is
+     *                              {@code null}.
+     */
+    public FilterCommand(Predicate<T> predicate,
+                         Function<Model, ItemManagerWithFilteredList<T>> managerAndListGetter) {
+        super(managerAndListGetter);
+        requireNonNull(predicate);
+        this.predicate = predicate;
+    }
+
+    @Override
+    public CommandResult execute(Model model) {
+        requireNonNull(model);
+
+        ItemManagerWithFilteredList<T> managerAndList = managerAndListGetter.apply(model);
+
+        managerAndList.updateFilteredItemsList(predicate);
+        return new CommandResult(
+                getResultOverviewMessage(managerAndList.getFilteredItemsList().size()));
+    }
+
+    /**
+     * Returns an overview message about the result of the filter operation, including the
+     * number of items that match the filter criteria.
+     */
+    public abstract String getResultOverviewMessage(int numberOfResults);
+}
