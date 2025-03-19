3e4156b93500976259b00f6348c8aec07ac019e8
NHT020305 <tungtun020305@gmail.com>
2025-03-19 23:05:49 +0800

    Add feature importing and exporting todo database

--- src/main/java/seedu/address/storage/todo/JsonAdaptedTodo.java
+++ src/main/java/seedu/address/storage/todo/JsonAdaptedTodo.java
@@ -16,6 +16,7 @@ import seedu.address.model.todo.Todo;
 import seedu.address.model.todo.TodoDeadline;
 import seedu.address.model.todo.TodoName;
 import seedu.address.model.todo.TodoLocation;
+import seedu.address.model.todo.TodoStatus;
 import seedu.address.storage.person.JsonAdaptedPerson;
 
 /**
@@ -28,6 +29,7 @@ public class JsonAdaptedTodo {
     private final String name;
     private final String deadline;
     private final String location;
+    private final String status;
     private final List<JsonAdaptedPerson> persons = new ArrayList<>();
 
     /**
@@ -38,10 +40,12 @@ public class JsonAdaptedTodo {
             @JsonProperty("name") String name,
             @JsonProperty("deadline") String deadline,
             @JsonProperty("location") String location,
+            @JsonProperty("status") String status,
             @JsonProperty("persons") List<JsonAdaptedPerson> persons){
         this.name = name;
         this.deadline = deadline;
         this.location = location;
+        this.status = status;
         if (persons != null) {
             this.persons.addAll(persons);
         }
@@ -54,6 +58,7 @@ public class JsonAdaptedTodo {
         name = source.getName().toString();
         deadline = source.getDeadline().toString();
         location = source.getLocation().toString();
+        status = source.getStatus().toString();
         persons.addAll(source.getPersons().stream()
                 .map(JsonAdaptedPerson::new)
                 .collect(Collectors.toList()));
@@ -70,15 +75,15 @@ public class JsonAdaptedTodo {
             todoPersons.add(person.toModelType());
         }
         if (name == null) {
-            throw new IllegalValueException(String.format(MISSING_FIELD_MESSAGE_FORMAT, Name.class.getSimpleName()));
+            throw new IllegalValueException(String.format(MISSING_FIELD_MESSAGE_FORMAT, TodoName.class.getSimpleName()));
         }
         if (!Name.isValidName(name)) {
-            throw new IllegalValueException(Name.MESSAGE_CONSTRAINTS);
+            throw new IllegalValueException(TodoName.MESSAGE_CONSTRAINTS);
         }
         final TodoName todoName = new TodoName(name);
 
         if (deadline == null) {
-            throw new IllegalValueException(String.format(MISSING_FIELD_MESSAGE_FORMAT, Phone.class.getSimpleName()));
+            throw new IllegalValueException(String.format(MISSING_FIELD_MESSAGE_FORMAT, TodoDeadline.class.getSimpleName()));
         }
         if (!TodoDeadline.isValid(deadline)) {
             throw new IllegalValueException(TodoDeadline.MESSAGE_CONSTRAINTS);
@@ -86,13 +91,17 @@ public class JsonAdaptedTodo {
         final TodoDeadline todoDeadline = new TodoDeadline(deadline);
 
         if (location == null) {
-            throw new IllegalValueException(String.format(MISSING_FIELD_MESSAGE_FORMAT, Email.class.getSimpleName()));
+            throw new IllegalValueException(String.format(MISSING_FIELD_MESSAGE_FORMAT, TodoLocation.class.getSimpleName()));
         }
         if (!TodoLocation.isValid(location)) {
-            throw new IllegalValueException(Email.MESSAGE_CONSTRAINTS);
+            throw new IllegalValueException(TodoLocation.MESSAGE_CONSTRAINTS);
         }
         final TodoLocation todoLocation = new TodoLocation(location);
-        return new Todo(todoName, todoDeadline, todoLocation, todoPersons);
+
+        boolean isDone = status.equals("Done");
+        final TodoStatus todoStatus = new TodoStatus(isDone);
+
+        return new Todo(todoName, todoDeadline, todoLocation, todoStatus, todoPersons);
     }
 
 }
