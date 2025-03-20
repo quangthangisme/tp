770fa09177721efb65c5451ba6fdd4f31a01da32
Brian Ku <48638231+kubrian@users.noreply.github.com>
2025-03-20 14:58:30 +0800

    Fix `help bye` and all aliases

--- src/main/java/seedu/address/logic/commands/ExitCommand.java
+++ src/main/java/seedu/address/logic/commands/ExitCommand.java
@@ -13,7 +13,8 @@ public class ExitCommand extends Command {
     public static final String COMMAND_WORD_KILL = "kill";
 
     public static final String MESSAGE_EXIT_ACKNOWLEDGEMENT = "Exiting Address Book as requested ...";
-    public static final String MESSAGE_USAGE = "exit/bye/quit/kill";
+    public static final String MESSAGE_USAGE = COMMAND_WORD_EXIT + ", " + COMMAND_WORD_BYE + ", " + COMMAND_WORD_QUIT
+        + ", " + COMMAND_WORD_KILL + ": Exits the program.\n";
 
     @Override
     public CommandResult execute(Model model) {
