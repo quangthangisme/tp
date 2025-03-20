6e3ef44e146398b957a5c1575d6689d02071c9c9
Brian Ku <48638231+kubrian@users.noreply.github.com>
2025-03-20 17:42:35 +0800

    Specify out of range for Event and Todo

--- src/main/java/seedu/address/logic/EventMessages.java
+++ src/main/java/seedu/address/logic/EventMessages.java
@@ -7,7 +7,7 @@ public class EventMessages {
     public static final String MESSAGE_INVALID_PERSON_DISPLAYED_INDEX =
             "The person index provided is invalid: %1$s";
     public static final String MESSAGE_INVALID_EVENT_DISPLAYED_INDEX =
-            "The event index provided is invalid";
+            "The event index provided is out of range.";
     public static final String MESSAGE_DUPLICATE_EVENT =
             "This event already exists";
     public static final String MESSAGE_NOT_REMOVED =
