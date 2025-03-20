73cbe93493998b0d80baff7bdb3d539ace00b73b
Brian Ku <48638231+kubrian@users.noreply.github.com>
2025-03-20 16:48:26 +0800

    Remove raising ParseException

--- src/test/java/seedu/address/logic/commands/CommandTestUtil.java
+++ src/test/java/seedu/address/logic/commands/CommandTestUtil.java
@@ -17,7 +17,6 @@ import java.util.List;
 import seedu.address.commons.core.index.Index;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.logic.commands.person.EditPersonCommand;
-import seedu.address.logic.parser.exceptions.ParseException;
 import seedu.address.model.Model;
 import seedu.address.model.person.NameContainsKeywordsPredicate;
 import seedu.address.model.person.Person;
@@ -97,8 +96,6 @@ public class CommandTestUtil {
             assertEquals(expectedModel, actualModel);
         } catch (CommandException ce) {
             throw new AssertionError("Execution of command should not fail.", ce);
-//        } catch (ParseException e) {
-//            throw new RuntimeException(e);
         }
     }
 
