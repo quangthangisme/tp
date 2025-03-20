74c0bdcd696bc58173cf51920e88118ff10b5ed7
Brian Ku <48638231+kubrian@users.noreply.github.com>
2025-03-20 17:24:21 +0800

    Fix minor typos

--- src/main/java/seedu/address/logic/commands/todo/ListTodoCommand.java
+++ src/main/java/seedu/address/logic/commands/todo/ListTodoCommand.java
@@ -1,5 +1,7 @@
 package seedu.address.logic.commands.todo;
 
+import static seedu.address.logic.parser.CliSyntax.TODO_COMMAND_WORD;
+
 import seedu.address.logic.abstractcommand.ListCommand;
 import seedu.address.model.Model;
 import seedu.address.model.todo.Todo;
@@ -12,7 +14,7 @@ public class ListTodoCommand extends ListCommand<Todo> {
     public static final String COMMAND_WORD = "list";
 
     public static final String MESSAGE_SUCCESS = "Listed all todos";
-    public static final String MESSAGE_USAGE = "list";
+    public static final String MESSAGE_USAGE = TODO_COMMAND_WORD + " " + COMMAND_WORD + ": Lists all todos.";
 
     /**
      * Creates a {@code ListTodoCommand}.
