529c926834801e0b0ebc34bab9e778cc56ac9cd7
Brian Ku <48638231+kubrian@users.noreply.github.com>
2025-03-27 22:09:04 +0800

    Change prefix for test

--- src/test/java/seedu/address/logic/commands/TodoCommandTestUtil.java
+++ src/test/java/seedu/address/logic/commands/TodoCommandTestUtil.java
@@ -1,8 +1,8 @@
 package seedu.address.logic.commands;
 
-import static seedu.address.logic.parser.todo.TodoCliSyntax.PREFIX_TODO_DEADLINE;
-import static seedu.address.logic.parser.todo.TodoCliSyntax.PREFIX_TODO_LOCATION;
-import static seedu.address.logic.parser.todo.TodoCliSyntax.PREFIX_TODO_NAME;
+import static seedu.address.logic.parser.todo.TodoCliSyntax.PREFIX_TODO_DEADLINE_LONG;
+import static seedu.address.logic.parser.todo.TodoCliSyntax.PREFIX_TODO_LOCATION_LONG;
+import static seedu.address.logic.parser.todo.TodoCliSyntax.PREFIX_TODO_NAME_LONG;
 
 /**
  * Contains helper methods for testing todo commands.
@@ -15,20 +15,20 @@ public class TodoCommandTestUtil {
     public static final String VALID_LOCATION_REPORT = "Canvas";
     public static final String VALID_LOCATION_GRADING = "Prof office";
 
-    public static final String NAME_DESC_REPORT = " " + PREFIX_TODO_NAME + VALID_NAME_REPORT;
-    public static final String NAME_DESC_GRADING = " " + PREFIX_TODO_NAME + VALID_NAME_GRADING;
+    public static final String NAME_DESC_REPORT = " " + PREFIX_TODO_NAME_LONG + VALID_NAME_REPORT;
+    public static final String NAME_DESC_GRADING = " " + PREFIX_TODO_NAME_LONG + VALID_NAME_GRADING;
     public static final String DEADLINE_DESC_REPORT =
-            " " + PREFIX_TODO_DEADLINE + VALID_DEADLINE_REPORT;
+            " " + PREFIX_TODO_DEADLINE_LONG + VALID_DEADLINE_REPORT;
     public static final String DEADLINE_DESC_GRADING =
-            " " + PREFIX_TODO_DEADLINE + VALID_DEADLINE_GRADING;
+            " " + PREFIX_TODO_DEADLINE_LONG + VALID_DEADLINE_GRADING;
     public static final String LOCATION_DESC_REPORT =
-            " " + PREFIX_TODO_LOCATION + VALID_LOCATION_REPORT;
+            " " + PREFIX_TODO_LOCATION_LONG + VALID_LOCATION_REPORT;
     public static final String LOCATION_DESC_GRADING =
-            " " + PREFIX_TODO_LOCATION + VALID_LOCATION_GRADING;
+            " " + PREFIX_TODO_LOCATION_LONG + VALID_LOCATION_GRADING;
 
-    public static final String INVALID_TODO_NAME_DESC = " " + PREFIX_TODO_NAME + "Stuff&stuff";
+    public static final String INVALID_TODO_NAME_DESC = " " + PREFIX_TODO_NAME_LONG + "Stuff&stuff";
     public static final String INVALID_TODO_DEADLINE_DESC_NOT_DATETIME =
-            " " + PREFIX_TODO_DEADLINE + "aaaa";
+            " " + PREFIX_TODO_DEADLINE_LONG + "aaaa";
     public static final String INVALID_TODO_DEADLINE_DESC_INCORRECT_FORMAT =
-            " " + PREFIX_TODO_DEADLINE + "23:59 25-12-31";
+            " " + PREFIX_TODO_DEADLINE_LONG + "23:59 25-12-31";
 }
