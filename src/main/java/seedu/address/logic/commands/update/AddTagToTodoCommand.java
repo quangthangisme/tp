7ed9ad352104b1245389ddb54abe7a4505c1da44
Brian Ku <48638231+kubrian@users.noreply.github.com>
2025-03-31 11:42:34 +0800

    Refactor AddTagToTodoCommand

--- src/main/java/seedu/address/logic/commands/update/AddTagToTodoCommand.java
+++ src/main/java/seedu/address/logic/commands/update/AddTagToTodoCommand.java
@@ -1,8 +1,6 @@
 package seedu.address.logic.commands.update;
 
 import static java.util.Objects.requireNonNull;
-import static seedu.address.logic.TodoMessages.MESSAGE_DUPLICATE_TODO;
-import static seedu.address.logic.TodoMessages.MESSAGE_INDEX_OUT_OF_RANGE_TODO;
 import static seedu.address.logic.parser.CliSyntax.TODO_COMMAND_WORD;
 import static seedu.address.logic.parser.todo.TodoCliSyntax.PREFIX_TODO_TAG_LONG;
 
@@ -19,7 +17,7 @@ import seedu.address.model.todo.Todo;
 /**
  * Adds a tag to a specified todo
  */
-public class AddTagToTodoCommand extends EditCommand<Todo> {
+public class AddTagToTodoCommand extends EditTodoCommand {
     public static final String COMMAND_WORD = "tag";
     public static final String MESSAGE_USAGE = TODO_COMMAND_WORD + " " + COMMAND_WORD
             + ": Adds a tag to a specified todo.\n"
@@ -30,46 +28,36 @@ public class AddTagToTodoCommand extends EditCommand<Todo> {
     public static final String MESSAGE_ADD_TAG_SUCCESS = "Added tag to todo: %1$s";
     public static final String MESSAGE_DUPLICATE_TAGS = "The tag is already assigned to this todo";
 
-    private final Tag tag;
-
     /**
      * Creates a AddTagToTodoCommand to add tags to a todo at a specific index.
      */
-    public AddTagToTodoCommand(Index index, Tag tag) {
-        super(index, Model::getTodoManagerAndList);
-        requireNonNull(tag);
-        this.tag = tag;
+    public AddTagToTodoCommand(Index index, EditTodoDescriptor editTodoDescriptor) {
+        super(index, editTodoDescriptor);
     }
 
     @Override
     public Todo createEditedItem(Model model, Todo todoToEdit) throws CommandException {
-        if (todoToEdit.getTags().contains(tag)) {
-            throw new CommandException(String.format(MESSAGE_DUPLICATE_TAGS));
+        requireNonNull(todoToEdit);
+        // Every tag that is intended to be added must not already be in the contact
+        Set<Tag> existingTags = todoToEdit.getTags();
+        Set<Tag> newTags = editTodoDescriptor.getTags().get();
+        for (Tag tag : newTags) {
+            if (existingTags.contains(tag)) {
+                throw new CommandException(String.format(MESSAGE_DUPLICATE_TAGS));
+            }
         }
-        Set<Tag> newTags = new HashSet<>(todoToEdit.getTags());
-        newTags.add(this.tag);
-        return new Todo(
-                todoToEdit.getName(),
-                todoToEdit.getDeadline(),
-                todoToEdit.getLocation(),
-                todoToEdit.getStatus(),
-                todoToEdit.getContacts(),
-                Set.copyOf(newTags)
-        );
-    }
 
-    @Override
-    public String getIndexOutOfRangeMessage() {
-        return MESSAGE_INDEX_OUT_OF_RANGE_TODO;
-    }
+        // Merge tags and update
+        Set<Tag> combinedTags = new HashSet<>(existingTags);
+        combinedTags.addAll(newTags);
+        editTodoDescriptor.setTags(combinedTags);
 
-    @Override
-    public String getDuplicateMessage() {
-        return MESSAGE_DUPLICATE_TODO;
+        return super.createEditedItem(model, todoToEdit);
     }
 
     @Override
     public String getSuccessMessage(Todo editedItem) {
         return String.format(MESSAGE_ADD_TAG_SUCCESS, Messages.format(editedItem));
     }
+
 }
--- src/main/java/seedu/address/logic/parser/todo/AddTagToTodoCommandParser.java
+++ src/main/java/seedu/address/logic/parser/todo/AddTagToTodoCommandParser.java
@@ -4,41 +4,47 @@ import static java.util.Objects.requireNonNull;
 import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.logic.parser.todo.TodoCliSyntax.PREFIX_TODO_TAG_LONG;
 
+import java.util.Set;
+
 import seedu.address.commons.core.index.Index;
 import seedu.address.logic.commands.update.AddTagToTodoCommand;
+import seedu.address.logic.commands.update.EditTodoDescriptor;
 import seedu.address.logic.parser.ArgumentMultimap;
 import seedu.address.logic.parser.ArgumentTokenizer;
 import seedu.address.logic.parser.Parser;
 import seedu.address.logic.parser.ParserUtil;
+import seedu.address.logic.parser.contact.ContactParserUtil;
 import seedu.address.logic.parser.exceptions.ParseException;
 import seedu.address.model.contact.Tag;
 
 /**
- * Parses input arguments and creates a new AddTagToTodoCommandParser object.
+ * Parses input arguments and creates a new AddTagToTodoCommand object
  */
 public class AddTagToTodoCommandParser implements Parser<AddTagToTodoCommand> {
     @Override
     public AddTagToTodoCommand parse(String args) throws ParseException {
         requireNonNull(args);
         ArgumentMultimap argMultimap = ArgumentTokenizer.tokenize(args, PREFIX_TODO_TAG_LONG);
-        if (!argMultimap.arePrefixesPresent(PREFIX_TODO_TAG_LONG)
-                || argMultimap.getPreamble().isEmpty()) {
-            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT,
-                    AddTagToTodoCommand.MESSAGE_USAGE));
+
+        // Ensure only one prefix is present
+        argMultimap.verifyNoDuplicatePrefixesFor(PREFIX_TODO_TAG_LONG);
+        if (!argMultimap.arePrefixesPresent(PREFIX_TODO_TAG_LONG) || argMultimap.getPreamble().isEmpty()) {
+            throw new ParseException(
+                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, AddTagToTodoCommand.MESSAGE_USAGE));
         }
+
         Index index;
         try {
             index = ParserUtil.parseIndex(argMultimap.getPreamble());
         } catch (ParseException pe) {
-            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT,
-                    AddTagToTodoCommand.MESSAGE_USAGE), pe);
+            throw new ParseException(
+                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, AddTagToTodoCommand.MESSAGE_USAGE), pe);
         }
-        String stringTag = argMultimap.getValue(PREFIX_TODO_TAG_LONG).get();
-        if (!Tag.isValidTagName(stringTag)) {
-            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT,
-                    Tag.MESSAGE_CONSTRAINTS));
-        }
-        Tag tag = new Tag(stringTag);
-        return new AddTagToTodoCommand(index, tag);
+
+        Set<Tag> tags = ContactParserUtil.parseTags(argMultimap.getAllValues(PREFIX_TODO_TAG_LONG));
+        EditTodoDescriptor editTodoDescriptor = new EditTodoDescriptor();
+        editTodoDescriptor.setTags(tags);
+
+        return new AddTagToTodoCommand(index, editTodoDescriptor);
     }
 }
