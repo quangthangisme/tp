722cd02e6e1cd9192075de986dfdd8e73f7684e4
raw-asparagus <66353964+raw-asparagus@users.noreply.github.com>
2025-04-01 13:06:20 +0800

    Change to deque

--- src/main/java/seedu/address/ui/CommandBox.java
+++ src/main/java/seedu/address/ui/CommandBox.java
@@ -1,5 +1,8 @@
 package seedu.address.ui;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
@@ -9,9 +12,6 @@ import javafx.scene.layout.Region;
 import seedu.address.logic.commands.CommandResult;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.logic.parser.exceptions.ParseException;
-
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * The UI component that is responsible for receiving user command inputs.
