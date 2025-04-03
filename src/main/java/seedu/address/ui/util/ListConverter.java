7bd12a8b64d2d6e0ccff050ddcb3861a7cfe202e
raw-asparagus <66353964+raw-asparagus@users.noreply.github.com>
2025-04-03 15:22:01 +0800

    Debloat separate `CardFactory` and `ItemAdpater`

--- src/main/java/seedu/address/ui/util/ListConverter.java
+++ src/main/java/seedu/address/ui/util/ListConverter.java
@@ -7,23 +7,32 @@ import javafx.collections.ObservableList;
 import seedu.address.model.contact.Contact;
 import seedu.address.model.event.Event;
 import seedu.address.model.todo.Todo;
-import seedu.address.ui.card.ContactCardFactory;
-import seedu.address.ui.card.EventCardFactory;
-import seedu.address.ui.card.TodoCardFactory;
+import seedu.address.ui.card.ContactCard;
+import seedu.address.ui.card.EventCard;
+import seedu.address.ui.card.GenericCardFactory;
+import seedu.address.ui.card.TodoCard;
 
 /**
  * Utility class to convert between different list types.
+ * Uses the generic adapter pattern to convert domain objects to displayable items.
  */
 public class ListConverter {
+    private static final GenericCardFactory<Contact> CONTACT_CARD_FACTORY =
+            new GenericCardFactory<>(ContactCard::new);
+
+    private static final GenericCardFactory<Event> EVENT_CARD_FACTORY =
+            new GenericCardFactory<>(EventCard::new);
+
+    private static final GenericCardFactory<Todo> TODO_CARD_FACTORY =
+            new GenericCardFactory<>(TodoCard::new);
 
     /**
      * Converts a list of Contact objects to a list of DisplayableItem objects.
      */
     public static ObservableList<DisplayableItem> toDisplayableContactList(ObservableList<Contact> contactList) {
-        ContactCardFactory factory = new ContactCardFactory();
         return FXCollections.observableList(
                 contactList.stream()
-                        .map(contact -> new ContactAdapter(contact, factory))
+                        .map(contact -> new GenericAdapter<>(contact, CONTACT_CARD_FACTORY))
                         .collect(Collectors.toList())
         );
     }
@@ -32,10 +41,9 @@ public class ListConverter {
      * Converts a list of Event objects to a list of DisplayableItem objects.
      */
     public static ObservableList<DisplayableItem> toDisplayableEventList(ObservableList<Event> eventList) {
-        EventCardFactory factory = new EventCardFactory();
         return FXCollections.observableList(
                 eventList.stream()
-                        .map(event -> new EventAdapter(event, factory))
+                        .map(event -> new GenericAdapter<>(event, EVENT_CARD_FACTORY))
                         .collect(Collectors.toList())
         );
     }
@@ -44,10 +52,9 @@ public class ListConverter {
      * Converts a list of Todo objects to a list of DisplayableItem objects.
      */
     public static ObservableList<DisplayableItem> toDisplayableTodoList(ObservableList<Todo> todoList) {
-        TodoCardFactory factory = new TodoCardFactory();
         return FXCollections.observableList(
                 todoList.stream()
-                        .map(todo -> new TodoAdapter(todo, factory))
+                        .map(todo -> new GenericAdapter<>(todo, TODO_CARD_FACTORY))
                         .collect(Collectors.toList())
         );
     }
