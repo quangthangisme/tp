baec6724110f62e72db46bffe1990d7a01a8714b
Brian Ku <48638231+kubrian@users.noreply.github.com>
2025-04-07 02:21:21 +0800

    Event end is strictly after event start

--- src/main/java/seedu/address/logic/EventMessages.java
+++ src/main/java/seedu/address/logic/EventMessages.java
@@ -6,6 +6,5 @@ package seedu.address.logic;
 public class EventMessages {
     public static final String MESSAGE_INDEX_OUT_OF_RANGE_EVENT = "The event index %1$s is out of range.";
     public static final String MESSAGE_DUPLICATE_EVENT = "This event already exists";
-    public static final String MESSAGE_NEGATIVE_DURATION =
-            "The end time must not be before the start time";
+    public static final String MESSAGE_NEGATIVE_DURATION = "The end time must be after the start time";
 }
